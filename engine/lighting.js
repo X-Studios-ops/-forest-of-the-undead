9ed3a379c0ad44fbc3b537dcd9589ff3c14a1218
Pratyush Ranjan Roul <som423428@gmail.com>
2026-08-02 12:17:37 +0530

    Refactor initLighting to remove camera parameter

--- engine/lighting.js
+++ engine/lighting.js
@@ -2,14 +2,23 @@ import * as THREE from 'three';
 
 let playerLight;
 
-export function initLighting(scene, camera) {
-    // Dim ambient moonlight
+export function initLighting(scene) {
+    // Dim moonlight ambient lighting
     const hemiLight = new THREE.HemisphereLight(0x444455, 0x111111, 0.5); 
     scene.add(hemiLight);
 
-    // Tactical Flashlight attached to the player
+    // Tactical Flashlight attached to player
     playerLight = new THREE.PointLight(0xffeedd, 1.5, 25);
     scene.add(playerLight);
-    
-    // (Optional) Function to update light position based on camera can be exported
+}
+
+export function updateLighting(camera) {
+    if (playerLight && camera) {
+        playerLight.position.copy(camera.position);
+        
+        // Offset the light to act like a gun barrel flashlight
+        const lightOffset = new THREE.Vector3(0.3, -0.2, -0.5);
+        lightOffset.applyQuaternion(camera.quaternion);
+        playerLight.position.add(lightOffset);
+    }
 }
