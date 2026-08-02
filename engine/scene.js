76fa8c066fbb06f2713389668907ceee13aa7f8a
Pratyush Ranjan Roul <som423428@gmail.com>
2026-08-02 12:15:12 +0530

    Update comments for scene initialization

--- engine/scene.js
+++ engine/scene.js
@@ -2,11 +2,12 @@ import * as THREE from 'three';
 
 export function initScene() {
     const scene = new THREE.Scene();
+    
+    // Pitch dark aesthetic
     scene.background = new THREE.Color(0x020202);
     
-    // Horror FogExp2 - objects gradually fade into darkness
+    // Thick fog to hide unrendered objects and build tension
     scene.fog = new THREE.FogExp2(0x020202, 0.04);
     
     return scene;
 }
-
